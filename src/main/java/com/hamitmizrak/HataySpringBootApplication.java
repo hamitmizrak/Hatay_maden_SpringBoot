4d68b7d0459a06236950fcc137d82a2f35651332
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-04 13:27:00 +0300

    Spring boot application

--- src/main/java/com/hamitmizrak/HataySpringBootApplication.java
+++ src/main/java/com/hamitmizrak/HataySpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HataySpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HataySpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HataySpringBootApplication.class, args);
+    }
 
 }
--- src/test/java/com/hamitmizrak/HataySpringBootApplicationTests.java
+++ src/test/java/com/hamitmizrak/HataySpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HataySpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
