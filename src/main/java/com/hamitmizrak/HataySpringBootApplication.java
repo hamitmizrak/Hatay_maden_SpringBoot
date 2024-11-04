c83b20507f7a407b9bcb8577fd254145c8ce8307
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-04 15:03:20 +0300

    persist url

--- src/main/java/com/hamitmizrak/HataySpringBootApplication.java
+++ src/main/java/com/hamitmizrak/HataySpringBootApplication.java
@@ -3,11 +3,63 @@ package com.hamitmizrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+// @ComponentScan("com")
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+// @EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme, çünkü Bcrypted kullancağım ancak Spring security için gerekli kütüphaneleri dahil
+// Buradaki exclude ne zaman kapatmam gerekiyor ? cevap: Spring Security ile çalıştığımız zaman kapat
+
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+
+//@SpringBootApplication
 public class HataySpringBootApplication {
 
+    // PSVM
     public static void main(String[] args) {
-        SpringApplication.run(HataySpringBootApplication.class, args);
-    }
 
-}
+        // devtools active isActive
+        System.setProperty("spring.devtools.restart.enabled", "true");
+
+        // PORT Ayarlamak
+        /*
+        65536 port
+        0<=PORT<=1023 80,443,23
+        1024<=PORT<=49151 5432,3306
+        49152<=PORT<=65535
+         */
+        /*
+        SpringApplication app = new SpringApplication(HataySpringBootApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "4444"));
+        app.run(args);
+         */
+
+        // JOptional pane aktif etmek
+        System.setProperty("java.awt.headless", "false");
+
+        // Main
+        SpringApplication.run(HataySpringBootApplication.class, args);
+    } // end PSVM
+} // end HataySpringBootApplication
