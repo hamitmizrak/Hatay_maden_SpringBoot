f4aafb41696daa69bad4cdd8b449904bb01e616b
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-05 10:12:44 +0300

    AddressDto

--- src/main/java/com/hamitmizrak/business/dto/AddressDto.java
+++ src/main/java/com/hamitmizrak/business/dto/AddressDto.java
@@ -1,4 +1,49 @@
 package com.hamitmizrak.business.dto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+import java.io.Serializable;
 
-public class AddressDto {
-}
+// LOMBOK
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+
+// Address(1) - Customer(1)
+public class AddressDto implements Serializable {
+
+    // SERILEŞTIRME
+    public static final Long serialVersionUID = 1L;
+
+    // FIELD
+    private Long id;
+
+    @NotEmpty(message = "{address.door_number.validation.constraints.NotNull.message}")
+    private String doorNumber;
+
+    @NotEmpty(message = "{address.street.validation.constraints.NotNull.message}")
+    private String street;
+
+    @NotEmpty(message = "{address.city.validation.constraints.NotNull.message}")
+    private String city;
+
+    @NotEmpty(message = "{address.state.validation.constraints.NotNull.message}")
+    private String state;
+
+    @NotEmpty(message = "{address.zip_code.validation.constraints.NotNull.message}")
+    private String zipCode;
+
+    @NotEmpty(message = "{address.qr_code.unique.validation.constraints.NotNull.message}")
+    private String addressQrCode;
+
+    @NotEmpty(message = "{address.description.validation.constraints.NotNull.message}")
+    @Size(min = 5,message = "{address.description.least.validation.constraints.NotNull.message}")
+    private String description;
+
+    // RELATION
+
+} //end class AddressDto
