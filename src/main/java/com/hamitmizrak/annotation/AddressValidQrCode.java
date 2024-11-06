88c1af5a5ddc439797e0e04d07b3d954efcdb7a1
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-06 16:16:28 +0300

    Annotation

--- src/main/java/com/hamitmizrak/annotation/AddressValidQrCode.java
+++ src/main/java/com/hamitmizrak/annotation/AddressValidQrCode.java
@@ -1,8 +1,6 @@
 package com.hamitmizrak.annotation;
 
-import com.hamitmizrak.data.entity.AddressEntity;
 import com.hamitmizrak.data.repository.IAddressRepository;
-import com.hamitmizrak.exception._404_NotFoundException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
@@ -12,19 +10,31 @@ import lombok.RequiredArgsConstructor;
 
 // Address Tablosundaki AddressQrCode eğer ilgili kayıt varsa;
 // eklemesin ve hata olarak benzersiz bir address oluştururun
-public class AddressValidQrCode implements ConstraintValidator<AddressUniqueQrCode,String> {
+public class AddressValidQrCode implements ConstraintValidator<AddressUniqueQrCode, String> {
 
     // INJECTION
     private final IAddressRepository iAddressRepository;
 
     @Override
     public boolean isValid(String addressQrCode, ConstraintValidatorContext constraintValidatorContext) {
-        String findQrCode= addressQrCode+ " Böyle bir qr code yoktur ";
+        String findQrCode = addressQrCode + " Böyle bir qr code yoktur ";
         // 1.YOL
+        /*
       AddressEntity addressEntity= iAddressRepository
               .findAddressEntityByAddressEntityEmbeddable_AddressQrCode(addressQrCode)
               .orElseThrow( () -> new _404_NotFoundException(findQrCode));
+         */
+
         // 2.YOL
-        return false;
-    }
-}
+        boolean isResult = iAddressRepository
+                .findAddressEntityByAddressEntityEmbeddable_AddressQrCode(addressQrCode)
+                .isPresent();
+        if (isResult) {
+            System.out.println("Aynı isimde Address Qr Code Vardır başka bir addres");
+            return false;
+        } else {
+            System.out.println("Harika farklı isimde Address Qr Code yazdınız");
+            return true;
+        } //end else
+    } //end isValid
+}//end AddressValidQrCode
