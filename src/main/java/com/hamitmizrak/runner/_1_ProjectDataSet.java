9da2bc97350ff1706d35b002750537c0050a732a
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-07 11:37:25 +0300

    CommandLineRunner Customer

--- src/main/java/com/hamitmizrak/runner/_1_ProjectDataSet.java
+++ src/main/java/com/hamitmizrak/runner/_1_ProjectDataSet.java
@@ -3,12 +3,13 @@ package com.hamitmizrak.runner;
 import com.hamitmizrak.business.dto.AddressDto;
 import com.hamitmizrak.business.dto.CustomerDto;
 import com.hamitmizrak.business.services.IAddressService;
+import com.hamitmizrak.business.services.ICustomerService;
+import com.hamitmizrak.data.repository.ICustomerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -26,6 +27,7 @@ public class _1_ProjectDataSet implements CommandLineRunner {
 
     // Injection
     private final IAddressService iAddressService;
+    private final ICustomerService iCustomerService;
 
     // AddressDto List Save
     private List<AddressDto> addressSave(){
@@ -45,12 +47,37 @@ public class _1_ProjectDataSet implements CommandLineRunner {
         return addressDtoList;
     }
 
-    private AddressDto saveAddress(AddressDto addressDto){
-        return null;
+    // Address Save
+    private AddressDto saveAddress(){
+        AddressDto addressDto = new AddressDto();
+        addressDto.setCity("Hatay ");
+        addressDto.setDescription("tanımlama ");
+        addressDto.setStreet("cadde ");
+        addressDto.setState("state ");
+        addressDto.setZipCode("zip code ");
+        addressDto.setDoorNumber("door number ");
+        addressDto.setAddressQrCode(UUID.randomUUID().toString());
+        return addressDto;
     }
 
-    private CustomerDto saveCustomer(CustomerDto customerDto){
-        return null;
+    private CustomerDto saveCustomer(){
+
+        // Address Dto Save
+        AddressDto addressDto = saveAddress();
+
+        // Customer Dto Save
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setFirstName("Hamit");
+        customerDto.setLastName("Mızrak");
+        customerDto.setNotes("Notes");
+
+        // Composition
+        customerDto.setAddressDto(addressDto);
+
+        // Save
+        iCustomerService.customerServiceCreate(customerDto);
+        System.out.println(customerDto);
+        return customerDto;
     }
 
     @Override
